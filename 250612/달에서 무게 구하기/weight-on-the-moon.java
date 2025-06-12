72d93f8f5f2504012d6a186742091e94339fd98b
jaemin-winter <ktd562@naver.com>
2025-06-12 09:45:03 +0900

    Title: 달에서 무게 구하기, Time: 107ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250612/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        double gram = 13;
+        double percentage = 0.165;
+
+        System.out.printf("%.0f * %.6f = %.6f", gram, percentage, gram * percentage);
+    }
+}
